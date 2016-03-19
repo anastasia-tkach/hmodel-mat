583eadb5e80d8097d20c8370b3ab8ca2f59cd04d
anastasia-tkach <tkach@intranet.epfl.ch>
2016-03-19 21:59:57 +0100

    htrack move

--- tests/T_2006_03_18_initial_transformation.m
+++ tests/T_2006_03_18_initial_transformation.m
@@ -1,6 +1,165 @@
-l1 = rand();
-l2 = rand();
-l3 = rand();
-alpha = randn(3, 1);
-theta = randn(4, 1);
+% l1 = rand();
+% l2 = rand();
+% l3 = rand();
+% alpha = randn(3, 1);
+% theta = randn(4, 1);
+clear; clc;
+
+%% Phalanges
+phalanges = cell(5, 1);
+% Hand
+phalanges{1}.local = [...
+    1         0         0 -0.597578
+    0         1         0   12.2038
+    0         0         1  -2.58477
+    0         0         0         1];
+phalanges{1}.parent_id = 6;
+phalanges{1}.children_ids = [2];
+% Thumb1
+phalanges{2}.local = ...
+    [0.353999  0.922935  0.151249   11.2015
+-0.922935  0.318587  0.216088  0.920158
+ 0.151249 -0.216088  0.964588  -6.98464
+        0         0         0         1];
+phalanges{2}.parent_id = 1;
+phalanges{2}.children_ids = [3];
+% Thumb2
+phalanges{3}.local = [...
+    0.854949     -0.427578      0.293664 -1.77636e-015
+     0.427126      0.901543     0.0691577       29.2982
+    -0.294321      0.066305      0.953404  1.33227e-015
+            0             0             0             1];
+phalanges{3}.parent_id = 2;
+phalanges{3}.children_ids = [4];
+% Thumb3
+phalanges{4}.local = [...
+    0.961957  -0.272905 -0.0127175   0.416739
+  0.259639   0.927698  -0.268261    20.4192
+ 0.0850077   0.254754   0.963262   -5.91231
+         0          0          0          1];
+phalanges{4}.parent_id = 3;
+phalanges{4}.children_ids = [];
+
+phalanges{5}.local = [...
+1 0 0 0
+0 1 0 0
+0 0 1 0
+0 0 0 1];
+phalanges{5}.children_ids = [6];
+phalanges{5}.parent_id = -1;
+
+phalanges{6}.local = [...
+1 0 0 0
+0 1 0 0
+0 0 1 0
+0 0 0 1];
+phalanges{6}.children_ids = [1];
+phalanges{6}.parent_id = 5;
+
+%% Dofs
+% 0
+index = 1;
+dofs = cell(13, 1);
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 1;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 1
+dofs{index}.axis = [0 1 0];
+dofs{index}.type = 1;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 2
+dofs{index}.axis = [0 0 1];
+dofs{index}.type = 1;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 3
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 4
+dofs{index}.axis = [0 1 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 5
+dofs{index}.axis = [0 0 1];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 5;
+index = index + 1;
+
+% 6
+dofs{index}.axis = [0 0 1];
+dofs{index}.type = 0;
+dofs{index}.phalange_id =  -1;
+index = index + 1;
+
+% 7
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id =  -1;
+index = index + 1;
+
+% 8
+dofs{index}.axis = [0 1 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id =  -1;
+index = index + 1;
+
+% 9
+dofs{index}.axis = [0 0 1];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 2;
+index = index + 1;
+
+% 10
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 2;
+index = index + 1;
+
+% 11
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 3;
+index = index + 1;
+
+% 12
+dofs{index}.axis = [1 0 0];
+dofs{index}.type = 0;
+dofs{index}.phalange_id = 4;
+index = index + 1;
+
+theta = [0, -70, 400, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+theta(5) = 1;
+theta(10) = 0.5;
+theta(11) = 0.3;
+theta(12) = 1;
+theta(13) = 0.5;
+phalanges = htrack_move(theta, dofs, phalanges);
+
+for i = 1:length(phalanges)
+    disp(i);    
+    disp('global  = ');
+    disp(phalanges{i}.global);
+end
+
+
+
+
+
+
+
+
+
+
+
+
 
--- tests/htrack_move.m
+++ tests/htrack_move.m
@@ -1,34 +1,63 @@
-function [] = htrack_move(theta)
+function [phalanges] = htrack_move(theta, dofs, phalanges)
+num_thetas = length(dofs);
+num_phalanges = 4;
 
-num_phalanges = 16;
-for i = 1:num_phalanges + 2
-	phalanges[i].local = phalanges[i].init_local;
-	}
+rotateX = zeros(num_thetas, 1);
+rotateZ = zeros(num_thetas, 1);
+rotateY = zeros(num_thetas, 1);
+globals = zeros(num_thetas, 1);
 
-	vector<float> rotateX(num_thetas, 0); // flexion
-	vector<float> rotateZ(num_thetas, 0); // abduction
-	vector<float> rotateY(num_thetas, 0); // twist
-	vector<float> globals(num_thetas, 0); // pose
+for i = 1:num_thetas
+    if (dofs{i}.phalange_id <= num_phalanges && dofs{i}.type == 0)
+        if all(dofs{i}.axis == [1, 0, 0])
+            rotateX(i) = theta(i);
+        elseif all(dofs{i}.axis == [0, 1, 0])
+            rotateY(i) = theta(i);
+        else all(dofs{i}.axis == [0, 0, 1])
+            rotateZ(i) = theta(i);
+        end          
+    else
+        globals(i) = theta(i);
+    end
+    
+end
+phalanges = transform_joints(globals, dofs, phalanges);
+phalanges = transform_joints(rotateX, dofs, phalanges);
+phalanges = transform_joints(rotateZ, dofs, phalanges);
+phalanges = transform_joints(rotateY, dofs, phalanges);
 
-	for (size_t i = 0; i < num_thetas; ++i) {
-		if (dofs[i].phalange_id < num_phalanges && dofs[i].type == ROTATION_AXIS) {
-			if (dofs[i].axis == Vec3f(1, 0, 0))
-				rotateX[i] = theta[i];
-			else if (dofs[i].axis == Vec3f(0, 1, 0))
-				rotateY[i] = theta[i];
-			else if (dofs[i].axis == Vec3f(0, 0, 1))
-				rotateZ[i] = theta[i];
-			else
-				cout << "wrong axis" << endl;
+end
 
-		}
-		else
-			globals[i] = theta[i];
-	}
+function [phalanges] = transform_joints(theta, dofs, phalanges)
+for i = 1:length(dofs)
+    if (dofs{i}.phalange_id == -1), continue; end
+    switch (dofs{i}.type)
+        case 1
+            phalanges = translate(dofs{i}.phalange_id, dofs{i}.axis * theta(i), phalanges);
+        case 0
+            phalanges = rotate(dofs{i}.phalange_id, dofs{i}.axis, theta(i), phalanges);
+    end
+end
+end
 
-	//transform joints separately
-	transform_joints(globals); // pose	
-	transform_joints(rotateX); // flexion
-	transform_joints(rotateZ); // abduction
-	transform_joints(rotateY); // twist
-}
+function [phalanges] = update(id, phalanges)
+if (phalanges{id}.parent_id >= 0)
+    phalanges{id}.global = phalanges{phalanges{id}.parent_id}.global * phalanges{id}.local;
+else
+    phalanges{id}.global = phalanges{id}.local;
+end
+for i = 1 : length(phalanges{id}.children_ids)
+    phalanges = update(phalanges{id}.children_ids(i), phalanges);
+end
+end
+
+function [phalanges] = translate(id, t, phalanges)
+phalanges{id}.local(1:3, 4) = phalanges{id}.local(1:3, 4) + t';
+phalanges = update(id, phalanges);
+end
+
+function [phalanges] = rotate(id, axis, angle, phalanges)
+phalanges{id}.local = phalanges{id}.local *  makehgtform('axisrotate', axis, angle);
+phalanges = update(id, phalanges);
+end
+
