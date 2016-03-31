2560606e14476d2c1e3e03aad18b514b213fbd96
anastasia-tkach <tkach@intranet.epfl.ch>
2016-03-31 14:34:19 +0200

    Ammend

--- cpp_interface/initial_transformations/test_initial_transformations.m
+++ cpp_interface/initial_transformations/test_initial_transformations.m
@@ -89,36 +89,40 @@ parameters5 = zeros(num_thetas, 1);
 num_alpha_thetas = 5 + num_poses * 4;
 lower_bound_thumb = -pi/2 * ones(num_alpha_thetas, 1);
 upper_bound_thumb = pi/2 * ones(num_alpha_thetas, 1);
-% lower_bound_thumb(1) = 0; upper_bound_thumb(1) = 1; 
-% lower_bound_thumb(2) = 0; upper_bound_thumb(2) = 1; 
-% lower_bound_thumb(3) = -1; upper_bound_thumb(3) = 1;  
+% lower_bound_thumb(1:3) = -0.1;
+% upper_bound_thumb(1:3) = 0.1;
+
 lower_bound_thumb(4:5) = - pi/3; upper_bound_thumb(4:5) = pi/3;
 
 lower_bound_fingers = -pi/9 * ones(num_alpha_thetas, 1);
-upper_bound_fingers = pi/9 * ones(num_alpha_thetas, 1);
+upper_bound_fingers = pi/2 * ones(num_alpha_thetas, 1);
+lower_bound_fingers(1:3) = -pi/9;
+upper_bound_fingers(1:3) = pi/4;
+lower_bound_fingers(4:5) = -pi/40;
+upper_bound_fingers(4:5) = pi/40;
 
 thumb_indices = [names_map('thumb_base'), names_map('thumb_bottom'), names_map('thumb_middle'), names_map('thumb_top')];
-[M1, M2, M3, L, theta_thumb] = compute_initial_transformation(poses, thumb_indices, lower_bound_thumb, upper_bound_thumb, []);%'thumb');
+[M1, M2, M3, L, theta_thumb] = compute_initial_transformation(poses, thumb_indices, lower_bound_thumb, upper_bound_thumb, 'thumb');
 phalanges{2}.local = M1; phalanges{3}.local = M2; phalanges{4}.local = M3;
 phalanges{2}.length = L(1); phalanges{3}.length = L(2); phalanges{4}.length = L(3);
 
 index_indices = [names_map('index_base'), names_map('index_bottom'), names_map('index_middle'), names_map('index_top')];
-[M1, M2, M3, L, theta_index] = compute_initial_transformation(poses, index_indices, lower_bound_fingers, upper_bound_thumb, []);%'index');
+[M1, M2, M3, L, theta_index] = compute_initial_transformation(poses, index_indices, lower_bound_fingers, upper_bound_fingers, 'index');
 phalanges{14}.local = M1; phalanges{15}.local = M2; phalanges{16}.local = M3;
 phalanges{14}.length = L(1); phalanges{15}.length = L(2); phalanges{16}.length = L(3);
 
 middle_indices = [names_map('middle_base'), names_map('middle_bottom'), names_map('middle_middle'), names_map('middle_top')];
-[M1, M2, M3, L, theta_middle] = compute_initial_transformation(poses, middle_indices, lower_bound_fingers, upper_bound_thumb, []);%'middle');
+[M1, M2, M3, L, theta_middle] = compute_initial_transformation(poses, middle_indices, lower_bound_fingers, upper_bound_fingers, 'middle');
 phalanges{11}.local = M1; phalanges{12}.local = M2; phalanges{13}.local = M3;
 phalanges{11}.length = L(1); phalanges{12}.length = L(2); phalanges{13}.length = L(3);
 
 ring_indices = [names_map('ring_base'), names_map('ring_bottom'), names_map('ring_middle'), names_map('ring_top')];
-[M1, M2, M3, L, theta_ring] = compute_initial_transformation(poses, ring_indices, lower_bound_fingers, upper_bound_thumb, []);%'ring');
+[M1, M2, M3, L, theta_ring] = compute_initial_transformation(poses, ring_indices, lower_bound_fingers, upper_bound_fingers, 'ring');
 phalanges{8}.local = M1; phalanges{9}.local = M2; phalanges{10}.local = M3;
 phalanges{8}.length = L(1); phalanges{9}.length = L(2); phalanges{10}.length = L(3);
 
 pinky_indices = [names_map('pinky_base'), names_map('pinky_bottom'), names_map('pinky_middle'), names_map('pinky_top')];
-[M1, M2, M3, L, theta_pinky] = compute_initial_transformation(poses, pinky_indices, lower_bound_fingers, upper_bound_thumb, []);%'pinky');
+[M1, M2, M3, L, theta_pinky] = compute_initial_transformation(poses, pinky_indices, lower_bound_fingers, upper_bound_fingers, 'pinky');
 phalanges{5}.local = M1; phalanges{6}.local = M2; phalanges{7}.local = M3;
 phalanges{5}.length = L(1); phalanges{6}.length = L(2); phalanges{7}.length = L(3);
 
@@ -162,19 +166,25 @@ phalanges = htrack_move(P{pose_id}, dofs, phalanges);
 
 phalanges = initialize_offsets(poses{pose_id}.centers, phalanges, names_map);
 
-figure; hold on; axis off; axis equal;
-display_skeleton(poses{pose_id}.centers, radii, blocks, [], false, 'g');
-display_result(poses{pose_id}.centers, [], [], blocks(1:29), radii, false, 1, 'big');
+%figure; hold on; axis off; axis equal;
+%display_skeleton(poses{pose_id}.centers, radii, blocks, [], false, 'g');
+%display_result(poses{pose_id}.centers, [], [], blocks(1:29), radii, false, 1, 'big');
 
 %% Rotate model
+scaling_factor = 1.5;
 theta = zeros(num_thetas, 1);
-theta(10) =  0.023069;
+theta(10) =  0;
 theta(11) = 0.5;
 theta(12) = -0.2;
 theta(13) = -0.2;
 theta(4) = -0.4;
 theta(5) = 2.7;
-[centers, phalanges] = rotate_initial_transformations(poses{pose_id}.centers, radii, blocks, phalanges, dofs, theta, names_map);
+
+radii{names_map('thumb_additional')} = 4.7;
+%radii{names_map('thumb_top')} = 6.8;
+%radii{names_map('thumb_middle')} = 6.5;
+
+[centers, radii, phalanges] = rotate_and_scale_initial_transformations(poses{pose_id}.centers, radii, blocks, phalanges, dofs, theta, scaling_factor, names_map);
 
 %% Save the final model
 output_path = '_my_hand/final/';
@@ -183,31 +193,33 @@ save([output_path, 'radii.mat'], 'radii');
 save([output_path, 'phalanges.mat'], 'phalanges');
 save([output_path, 'dofs.mat'], 'dofs');
 
-display_result(centers, [], [], blocks(1:29), radii, false, 1, 'big'); view([-180, -90]); camlight;
+display_result(centers, [], [], blocks(1:28), radii, false, 1, 'big'); view([-180, -90]); camlight;
 
 %% Write model to cpp
 I = zeros(length(phalanges), 4 * 4);
 for i = 1:length(phalanges)
-    disp(phalanges{i}.local);
     I(i, :) = phalanges{i}.local(:)';
 end
 I = I';
 num_centers = 34;
-num_blocks = 29;
+num_blocks = 28;
 RAND_MAX = 32767;
 R = zeros(1, num_centers);
 C = zeros(D, num_centers);
-B = RAND_MAX * ones(3, length(blocks));
+B = RAND_MAX * ones(3, num_blocks);
 scaling_factor = 1;
 for j = 1:num_centers
     R(j) =  radii{j};
     C(:, j) = centers{j};
 end
-for j = 1:length(blocks)
+for j = 1:num_blocks
     for k = 1:length(blocks{j})
         B(k, j) = blocks{j}(k) - 1;
     end
 end
+
+path = 'C:\Developer\hmodel-cuda-build\data\hmodel\';
+write_input_parameters_to_files(path, C, R, B, I);
 
 % path = 'C:\Developer\hmodel-cuda-build\data\';
 % write_input_parameters_to_files(path, C, R, B, I);
